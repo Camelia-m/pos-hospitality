3703268cd50ac61de10fcb5e8b62e2f705f67ba8
Camelia-m <mirkamali.kamelia@gmail.com>
2025-12-21 12:14:22 +0100

    Prepare for cloud deployment

--- order-service/src/main/java/com/pao/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/pao/order/OrderServiceApplication.java
@@ -2,8 +2,13 @@ package com.pao.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        KafkaAutoConfiguration.class
+})
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
